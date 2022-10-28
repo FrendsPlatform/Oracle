467d5091c7258c3289c512138b3ba6b4f97ff850
Riku Virtanen <virtanen.riku91@gmail.com>
2022-10-28 09:29:11 +0300

    Changed query field DataFormatString to Sql

--- Frends.Oracle.ExecuteQuery/Frends.Oracle.ExecuteQuery/Definitions/Input.cs
+++ Frends.Oracle.ExecuteQuery/Frends.Oracle.ExecuteQuery/Definitions/Input.cs
@@ -11,20 +11,25 @@ namespace Frends.Oracle.ExecuteQuery.Definitions
         /// <summary>
         /// Query to be executed in string format.
         /// </summary>
-        /// <example>"SELECT ColumnName FROM TableName"</example>
-        [DisplayFormat(DataFormatString = "Text")]
-        [DefaultValue("SELECT ColumnName FROM TableName")]
+        /// <example>"INSERT INTO MyTable (id, first_name, last_name) VALUES (:id, :first_name, :last_name)"</example>
+        [DisplayFormat(DataFormatString = "Sql")]
+        [DefaultValue("INSERT INTO MyTable (id, first_name, last_name) VALUES (:id, :first_name, :last_name)")]
         public string Query { get; set; }
 
         /// <summary>
         /// Parameters for the database query.
         /// </summary>
-        /// <example>{ Name = "name", Value = "Matti", DataType = QueryParameterType.Varchar2 }</example>
+        /// <example>[
+        /// { Name = "id", Value = 1, DataType = QueryParameterType.Int32 },
+        /// { Name = "first_name", Value = "John", DataType = QueryParameterType.Varchar2 },
+        /// { Name = "last_name", Value = "Doe", DataType = QueryParameterType.Varchar2 }
+        /// ]</example>
         public QueryParameter[] Parameters { get; set; }
 
         /// <summary>
         /// Oracle connection string.
         /// </summary>
+        /// <example>Data Source=(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=MyHost)(PORT=MyPort))(CONNECT_DATA=(SERVICE_NAME=MyOracleSID)));User Id=myUsername;Password=myPassword;</example>
         [DisplayFormat(DataFormatString = "Text")]
         [PasswordPropertyText]
         [DefaultValue("Data Source=(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=MyHost)(PORT=MyPort))(CONNECT_DATA=(SERVICE_NAME=MyOracleSID)));User Id=myUsername;Password=myPassword;")]
