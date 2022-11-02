fe79d09c6cfb01ace2d33494379cd0973a8e165b
Riku Virtanen <virtanen.riku91@gmail.com>
2022-11-02 09:03:48 +0200

    PR review changes

--- Frends.Oracle.ExecuteQuery/Frends.Oracle.ExecuteQuery/Definitions/Result.cs
+++ Frends.Oracle.ExecuteQuery/Frends.Oracle.ExecuteQuery/Definitions/Result.cs
@@ -1,6 +1,4 @@
-﻿using Newtonsoft.Json.Linq;
-
-namespace Frends.Oracle.ExecuteQuery.Definitions;
+﻿namespace Frends.Oracle.ExecuteQuery.Definitions;
 
 /// <summary>
 /// Return object with private setters.
